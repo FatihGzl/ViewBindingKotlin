8abbd106b3f175832fd91f6bc5d323c14bf5d82e
FatihGzl <f.al-guzel-51@hotmail.com>
2024-05-30 01:42:31 +0300

    ViewBindingKotlin committed.

---
+++ app/src/main/java/com/example/viewbindingkotlin/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.viewbindingkotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewbindingkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener{
+
+        }
+
+        binding.textView.text = "Text"
+        binding.editText.setText("EditText Test")
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
